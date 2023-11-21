5411dc5a0e8709d6efa888d14c35206d713842f5
dhirlyrmdhn <dhirlyramadhan70@smp.belajar.id>
2023-11-21 20:23:24 +0700

    update 4

--- src/script/themes.js
+++ src/script/themes.js
@@ -1,1 +1,17 @@
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+function setTheme(theme) {
+  document.body.classList.toggle('dark-mode', theme === 'dark');
+  document.body.classList.toggle('light-mode', theme !== 'dark');
+}
+
+function toggleTheme() {
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  setTheme(isDarkMode ? 'light' : 'dark');
+}
+
+setTheme(prefersDarkMode ? 'dark' : 'light');
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+  setTheme(e.matches ? 'dark' : 'light');
+});
