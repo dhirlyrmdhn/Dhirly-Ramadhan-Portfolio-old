9728b3e09fc5e879f5dc66b719f4d7e5ddcd1a0d
dhirlyrmdhn <dhirlyramadhan70@smp.belajar.id>
2023-11-21 20:13:27 +0700

    update 1

--- script/preloader.js
+++ script/preloader.js
@@ -1,1 +1,31 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
 
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.toggle('active');
+            }, (idx + 1) * 300)
+        });
+
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
+
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.toggle('fade');
+                }, (idx + 1) * 50 )
+            })
+        }, 6000);
+
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 6500)
+
+    })
+
+})
