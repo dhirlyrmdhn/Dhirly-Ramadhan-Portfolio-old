544c940c47073bec1c808e6b1c75029c30502e63
dhirlyrmdhn <dhirlyramadhan70@smp.belajar.id>
2023-12-16 19:38:57 +0700

    20.6

--- src/script/navbar.js
+++ src/script/navbar.js
@@ -8,11 +8,11 @@ window.addEventListener("scroll", () => {
     let navMenuToggle = document.getElementById("menu-toggle");
 
     preloaderBottomContent.classList.remove("active", window.scrollY >= 1);
-    preloaderThemesMode.classList.remove("active", window.scrollY >= 1);
+    // preloaderThemesMode.classList.remove("active", window.scrollY >= 1);
     preloader.classList.toggle("nav", window.scrollY >= 1);
     preloaderTitle.classList.toggle("navbar-title", window.scrollY >= 1);
 
-    navMenuToggle.classList.toggle("active", window.scrollY >= 1);
+    // navMenuToggle.classList.toggle("active", window.scrollY >= 1);
     
 });
 
