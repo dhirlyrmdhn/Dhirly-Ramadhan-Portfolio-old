c6db61960ceb9899a02e6c6bec280c8bef1c917c
dhirlyrmdhn <dhirlyramadhan70@smp.belajar.id>
2023-12-20 18:57:49 +0700

    90.11

---
+++ src/script/cursor.js
@@ -0,0 +1,46 @@
+var cursor = document.querySelector(".cursor"),
+    cursorScale = document.querySelectorAll('.cursor-scale'),
+    mouseX = 0,
+    mouseY = 0
+
+gsap.to({}, 0.016, {
+    repeat: -1,
+    duration: 1,
+    ease: 'power2.ease',
+
+    onRepeat: function () {
+        gsap.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            },
+            ease: 'slow(0.7, 0.7, false)',
+            delay: 0.05
+        })
+    }
+});
+
+window.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY
+});
+window.addEventListener("mouseout", () => {
+    cursor.classList.add("hidden");
+})
+window.addEventListener("mouseover", () => {
+    cursor.classList.remove("hidden");
+})
+
+cursorScale.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        cursor.classList.remove('grow');
+        cursor.classList.remove('grow-small');
+    });
+    link.addEventListener('mousemove', () => {
+        cursor.classList.add('grow');
+        if(link.classList.contains('small')){
+            cursor.classList.remove('grow');
+            cursor.classList.add('grow-small');
+        }
+    })
+})
