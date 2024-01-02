319fb431db0812fb3086972e4b00a5edae4cdb53
dhirlyrmdhn <dhirlyramadhan70@smp.belajar.id>
2024-01-02 19:42:39 +0700

    revisi-3.0

--- src/script/preloader.js
+++ src/script/preloader.js
@@ -1,19 +1,29 @@
 let preloaderWord = document.querySelectorAll('.preloader-title-word');
+let preloaderDummy = document.querySelectorAll('.preloader-dummy');
 
 window.addEventListener("DOMContentLoaded", () => {
 
-    setTimeout(() => {
-        let preloaderDummy = document.querySelectorAll('.preloader-dummy');
-        preloaderDummy.forEach(preloaderDummy => {
-            preloaderDummy.classList.add('animation')
-        });
-    }, 500);
+    if (window.scrollY === 0) {
+        setTimeout(() => {
+            preloaderDummy.forEach(preloaderDummy => {
+                preloaderDummy.classList.add('animation')
+            });
+        }, 500);
 
-    setTimeout(() => {
-        preloaderWord.forEach((preloaderWord, idx) => {
-            setTimeout(() => {
-                preloaderWord.classList.add('animation');
-            }, (idx + 1) * 100);
+        setTimeout(() => {
+            preloaderWord.forEach((preloaderWord, idx) => {
+                setTimeout(() => {
+                    preloaderWord.classList.add('animation');
+                }, (idx + 1) * 100);
+            });
+        }, 1800);
+    } else {
+        // preloaderDummy.forEach(preloaderDummy => {
+        //     preloaderDummy.classList.add('animation')
+        // });
+        preloaderWord.forEach(preloaderWord => {
+            preloaderWord.classList.add('animation')
         });
-    }, 1800);
+    }
+
 });
